7fa250b490c468d9dfccd9026195fcd9bdad4070
Sonali <sonalimoharana133@gmail.com>
2024-03-12 21:55:32 +0530

    Crud Api for category

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -5,24 +5,75 @@ import { Subcategory } from "../models/subcategoryModel.js";
 import { Category } from "../models/categoryModel.js";
 
 const addCategoryAndSubcategory = asyncHandler(async (req, res) => {
-    try {
-      const { categoryData, subcategoryData } = req.body;
-  
-      // Create new subcategory instance
-      const subcategory = new Subcategory(subcategoryData);
-      await subcategory.save();
-  
-      // Create new category instance and add subcategory reference
-      const category = new Category(categoryData);
-      category.subcategories.push(subcategory);
-      await category.save();
-      return res
-      .status(200)
-      .json( new ApiResponse(200, "Category and subcategory created successfully" ));
-    } catch (error) {
-      console.error("Error creating category and subcategory:", error);
-      throw new ApiError(500,"Internal server error" );
-    }
-  });
+  try {
+    const { categoryData, subcategoryData } = req.body;
 
-  export {addCategoryAndSubcategory};
+    // Create new subcategory instance
+    const subcategory = new Subcategory(subcategoryData);
+    await subcategory.save();
+
+    // Create new category instance and add subcategory reference
+    const category = new Category(categoryData);
+    category.subcategories.push(subcategory);
+    await category.save();
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, "Category and subcategory created successfully")
+      );
+  } catch (error) {
+    console.error("Error creating category and subcategory:", error);
+    throw new ApiError(500, "Internal server error");
+  }
+});
+
+const getCategories = asyncHandler(async (req, res) => {
+  try {
+    const categories = await Category.find().populate("subcategories");
+    return res.status(200).json(new ApiResponse(200, { categories }));
+  } catch (error) {
+    console.error("Error creating category and subcategory:", error);
+    throw new ApiError(500, error.message);
+  }
+});
+
+const updateCategory = asyncHandler(async (req, res) => {
+  try {
+    const categoryId = req.params.categoryId;
+    const updatedCategory = await Category.findByIdAndUpdate(
+      categoryId,
+      req.body,
+      { new: true }
+    );
+    if (!updatedCategory) {
+      throw new ApiError(404, "Category not found");
+    }
+    return res
+      .status(200)
+      .json(new ApiResponse(200, { updatedCategory }, "Updated successfully"));
+  } catch (error) {
+    throw new ApiError(500, error.message);
+  }
+});
+
+const deleteCategory = asyncHandler(async (req, res) => {
+  try {
+    const categoryId = req.params.categoryId;
+    const deletedCategory = await Category.findByIdAndDelete(categoryId);
+    if (!deletedCategory) {
+      throw new ApiError(404, "Category not found");
+    }
+    // Also delete subcategories associated with this category
+    await Subcategory.deleteMany({ category: categoryId });
+    res.json({ message: "Category deleted successfully" });
+  } catch (error) {
+    throw new ApiError(500, error.message);
+  }
+});
+
+export {
+  addCategoryAndSubcategory,
+  getCategories,
+  updateCategory,
+  deleteCategory,
+};
--- src/routes/categoryrouter.js
+++ src/routes/categoryrouter.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { addCategoryAndSubcategory } from "../controllers/categoryController.js";
+import { addCategoryAndSubcategory, deleteCategory, getCategories, updateCategory } from "../controllers/categoryController.js";
 
 const router = Router();
 
 
 router.route("/addCategoryAndSubcategory").post(addCategoryAndSubcategory);
+router.route("/getCategories").get(getCategories);
+router.route("/updateCategory/:categoryId").put(updateCategory);
+router.route("/deleteCategory/:categoryId").delete(deleteCategory);
+
 export default router;
